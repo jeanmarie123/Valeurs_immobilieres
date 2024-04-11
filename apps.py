fb202076c5b2dfa5405ad79d8792158c359f0661
jean <jabengzoa@gmail.com>
2024-04-11 17:54:38 +0200

    reglage

--- apps.py
+++ apps.py
@@ -14,15 +14,15 @@ st.subheader(" :bar_chart: Tendance de l'immobilier en France ")
 
 tab1, tab2 = st.tabs(["Cat", "Dog"])
 
-df = data()
+
 
 with tab1:
     input_text = st.text_area(label = "entrez votre input")
     st.write(input_text)
-    st.dataframe(df)
+    st.dataframe(df_data)
 
 
 with tab2:
     input_v = st.text_area(label = "entrez votre code")
     st.write(input_v)
-    st.dataframe(df)
+    st.dataframe(df_data)
