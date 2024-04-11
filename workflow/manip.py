1e65ebe29e40d400caa0c14287a6868295e36010
jean <jabengzoa@gmail.com>
2024-04-11 18:01:25 +0200

    reglage_app

--- workflow/manip.py
+++ workflow/manip.py
@@ -2,9 +2,12 @@
 import duckdb 
 import pandas
 
-#con = duckdb.connect(database = "data/real_estate.db", read_only = False)
+con = duckdb.connect(database = "data/real_estate.db", read_only = False)
+df_data = con.execute("SELECT * FROM zone_geographique").df()
 
+""" 
 def data():
     with duckdb.connect(database = "data/real_estate.db", read_only = False) as con:
        df_data = con.execute("SELECT * FROM zone_geographique").df()
     return df_data 
+"""
